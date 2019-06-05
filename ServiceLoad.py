a08cb9944d2465a75f3cc91e656ab5e548b451d6
quintinnicolas <47981178+quintinnicolas@users.noreply.github.com>
2019-06-05 17:38:34 +0200

    Better handling of datetime

--- ServiceLoad.py
+++ ServiceLoad.py
@@ -10,5 +10,5 @@ def LoadService(startdate,enddate):
     import pandas as pd
     url = 'https://publications.elia.be/Publications/Publications/STLForecasting.v1.svc/ExportSTLFForecastGraph?fromDate=' + startdate +'T23%3A00%3A00.000Z&toDate=' + enddate + 'T23%3A00%3A00.000Z'
     load = pd.read_excel(url)
-    load.index = pd.to_datetime(load.DateTime)
+    load.index = pd.to_datetime(load.DateTime, dayfirst=True)
     return load
