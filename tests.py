d36e53ab36490dc1f57ce040ecb64cfea32c5602
Quintin Nicolas <nicolas.quintin@gogoro.com>
2021-12-23 14:52:22 +0800

    major upgrade
    - use requests package
    - use pytest
    - clean code

--- tests.py
+++ tests.py
@@ -1,35 +1,66 @@
 import pytest
 import datetime as dt
 from elia import elia
+from pytz import timezone
+
+BRU = timezone("Europe/Brussels")
+
+start_1 = dt.datetime.today() - dt.timedelta(days=2)
+end_1 = dt.datetime.today() - dt.timedelta(days=1)
+
+start_2 = dt.datetime.now(tz=BRU) - dt.timedelta(hours=12)
+end_2 = dt.datetime.now(tz=BRU)
 
 
 @pytest.fixture()
-def elia_client() -> elia.EliaClient:
+def elia_client() -> elia.EliaPandasClient:
     """Creates EliaClient object"""
-    end = dt.datetime.today()
-    start = end - dt.timedelta(days=2)
-    return elia.EliaClient(start, end)
+    return elia.EliaPandasClient()
 
 
-def test_forecast_wind(elia_client):
+@pytest.mark.parametrize("start, end", [(start_1, end_1), (start_2, end_2)])
+def test_forecast_wind(elia_client, start, end):
     """Testing wind query"""
-    df_test = elia_client.get_forecast_wind()
+    df_test = elia_client.get_forecast_wind(start, end)
+    number_of_quarter_hours = (end-start).days * 24 * 4 + (end-start).seconds // 900
     print(df_test.tail())
-    assert(len(df_test) >= 96)
+    assert(len(df_test) >= number_of_quarter_hours)
 
 
-def test_forecast_solar(elia_client):
+@pytest.mark.parametrize("start, end", [(start_1, end_1), (start_2, end_2)])
+def test_forecast_solar(elia_client, start, end):
     """Testing solar query"""
-    df_test = elia_client.get_forecast_solar()
+    df_test = elia_client.get_forecast_solar(start, end)
+    number_of_quarter_hours = (end-start).days * 24 * 4 + (end-start).seconds // 900
     print(df_test.tail())
-    assert(len(df_test) >= 96)
+    assert(len(df_test) >= number_of_quarter_hours)
 
 
-def test_forecast_load(elia_client):
+@pytest.mark.parametrize("start, end", [(start_1, end_1), (start_2, end_2)])
+def test_forecast_load(elia_client, start, end):
     """Testing consumption query"""
-    df_test = elia_client.get_forecast_load()
+    df_test = elia_client.get_forecast_load(start, end)
+    number_of_quarter_hours = (end-start).days * 24 * 4 + (end-start).seconds // 900
     print(df_test.tail())
-    assert(len(df_test) >= 96)
+    assert(len(df_test) >= number_of_quarter_hours)
+
+
+@pytest.mark.parametrize("start, end", [(start_1, end_1), (start_2, end_2)])
+def test_actual_imbalance_price_per_quarter(elia_client, start, end):
+    """Testing imbalance price query"""
+    df_test = elia_client.get_actual_imbalance_prices_per_quarter(start, end)
+    number_of_quarter_hours = (end-start).days * 24 * 4 + (end-start).seconds // 900
+    print(df_test.tail())
+    assert(len(df_test) >= number_of_quarter_hours)
+
+
+@pytest.mark.parametrize("start, end", [(start_1, end_1), (start_2, end_2)])
+def test_actual_imbalance_price_per_quarter_via_excel(elia_client, start, end):
+    """Testing imbalance price query"""
+    df_test = elia_client.get_actual_imbalance_prices_per_quarter_via_excel(start, end)
+    number_of_quarter_hours = (end-start).days * 24 * 4 + (end-start).seconds // 900
+    print(df_test.tail())
+    assert(len(df_test) >= number_of_quarter_hours)
 
 
 def test_actual_imbalance_volume(elia_client):
@@ -37,20 +68,6 @@ def test_actual_imbalance_volume(elia_client):
     df_test = elia_client.get_actual_imbalance_volume()
     print(df_test.tail())
     assert(len(df_test) >= 59)  # data per minute, only for the latest hour
-
-
-def test_actual_imbalance_price_per_quarter(elia_client):
-    """Testing imbalance price query"""
-    df_test = elia_client.get_actual_imbalance_prices_per_quarter()
-    print(df_test.tail())
-    assert(len(df_test) >= 96)
-
-
-def test_actual_imbalance_price_per_quarter_via_excel(elia_client):
-    """Testing imbalance price query"""
-    df_test = elia_client.get_actual_imbalance_prices_per_quarter_via_excel()
-    print(df_test.tail())
-    assert(len(df_test) >= 96)
 
 
 def test_actual_imbalance_price_per_minute(elia_client):
