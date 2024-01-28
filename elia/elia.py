8cc0b5680d08d97fac12245f6b6b448cd60e9b04
quintinnicolas <47981178+quintinnicolas@users.noreply.github.com>
2024-01-28 10:38:00 +0100

    remove unused constants TODAY and YESTERDAY

--- elia/elia.py
+++ elia/elia.py
@@ -13,8 +13,6 @@ import requests
 from elia.decorators import split_along_time
 
 DATETIME_FORMAT = "%Y-%m-%d %H:%S"
-TODAY = dt.date.today()
-YESTERDAY = TODAY - dt.timedelta(days=1)
 
 
 class EliaPandasClient:
